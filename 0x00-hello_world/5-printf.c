68fd78ebd66fb74ac67a5515bccca6770d91ec94
karisdalton <daltonkaris2000@gmail.com>
2022-09-10 11:55:41 -0700

    linting error fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
  * main - prints the phrase passed to printf
  * return 0 if properly exited, non-zero otherwise
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
